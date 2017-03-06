f704e4c36b62b2f540f0739a18780df538b6c7b5
WQ <869932084@qq.com>
2017-03-06 09:04:08 +0800

    Initial Commit

---
+++ VOA_English/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VOA_English
+//
+//  Created by 王强 on 17/3/6.
+//  Copyright © 2017年 王强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VOA_English/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VOA_English
+//
+//  Created by 王强 on 17/3/6.
+//  Copyright © 2017年 王强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
